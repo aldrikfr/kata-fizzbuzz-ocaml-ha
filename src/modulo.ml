c5a5c5766b30cdf582e5f0b30b860f28426d7926
Aldrik KLEBER <contact@aldrik.net>
2019-10-15 10:18:40 +0200

    more concise code

--- src/modulo.ml
+++ src/modulo.ml
@@ -6,9 +6,7 @@ type rule_description = Int.t * String.t
 
 let is_mod m n = n % m = 0
 
-let apply n acc {divisor; message} =
-  if n |> is_mod divisor then String.concat [acc; message] else acc
+let apply n acc rule =
+  if n |> is_mod rule.divisor then String.concat [acc; rule.message] else acc
 
-let create_rule (desc : rule_description) =
-  let divisor, message = desc in
-  {divisor; message}
+let create_rule (divisor, message) = {divisor; message}
